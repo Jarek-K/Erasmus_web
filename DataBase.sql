850d1475944df5a0b908699cfc99a22b5fc710ad
Jarek-K <kuszcz95@gmail.com>
2019-11-19 16:14:06 +0000

    db_new

--- DataBase.sql
+++ DataBase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 19 Lis 2019, 16:24
+-- Czas generowania: 19 Lis 2019, 17:13
 -- Wersja serwera: 10.1.38-MariaDB
 -- Wersja PHP: 7.3.2
 
@@ -40,6 +40,37 @@ CREATE TABLE `races` (
   `status` varchar(8) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Zrzut danych tabeli `races`
+--
+
+INSERT INTO `races` (`id`, `gender`, `slots`, `localization`, `age_min`, `age_max`, `deadline`, `race_date`, `status`) VALUES
+(1, 'ALL', 16, 'Madrid', 16, 40, '2019-11-19 16:00:51', '2019-11-30 00:00:00', 'OPEN'),
+(2, 'ALL', 16, 'Paris', 16, 40, '2019-11-19 16:00:47', '2019-11-30 00:00:00', 'OPEN');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `race_signup`
+--
+
+CREATE TABLE `race_signup` (
+  `race_id` int(16) NOT NULL,
+  `user_id` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `token`
+--
+
+CREATE TABLE `token` (
+  `token` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `user` int(11) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +99,20 @@ ALTER TABLE `races`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `race_signup`
+--
+ALTER TABLE `race_signup`
+  ADD KEY `race_id_a` (`race_id`),
+  ADD KEY `user_id_a` (`user_id`);
+
+--
+-- Indeksy dla tabeli `token`
+--
+ALTER TABLE `token`
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id_token` (`user`);
+
+--
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
@@ -83,13 +128,30 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `races`
 --
 ALTER TABLE `races`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `race_signup`
+--
+ALTER TABLE `race_signup`
+  ADD CONSTRAINT `race_id_a` FOREIGN KEY (`race_id`) REFERENCES `races` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id_a` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `user_id_token` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
